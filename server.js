4a39de2ca238cbfe7afe9f0e8f14f5ca81eadb25
SaadEddine Dahmi <dahmisaad7@gmail.com>
2022-12-23 08:40:51 +0100

    first

---
+++ server.js
@@ -0,0 +1,96 @@
+const mysql = require('mysql2');
+const { response } = require("express");
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs")
+
+app.use(express.static("static"))
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.get("/", function (req, res) {
+    res.render("index")
+    console.log('index is working')
+
+
+})
+app.get("/signup", function (req, res) {
+    res.render("signup")
+    console.log('signup is working')
+})
+
+
+app.get("/login", function (req, res) {
+    res.render("login")
+    console.log('login is working')
+})
+
+app.get("/templates", function (req, res) {
+    res.render("templates")
+    console.log('templates is working')
+
+})
+
+app.get("/formule1", function (req, res) {
+    res.render("formule1")
+    console.log('formule1 is working')
+
+
+})
+
+app.get("/formule2", function (req, res) {
+    res.render("formule2")
+    console.log('formule2 is working')
+
+
+})
+const conn = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'Izjz5699@#',
+    database : 'signup-nodejs'
+ });
+
+conn.connect((error)=>{
+    if (error) {
+        throw error;
+    } else{
+    console.log('Connected')
+}
+ });
+
+
+ app.post("/login", function(req,res){
+    const { email, password} = req.body;
+    conn.query("SELECT * FROM signup WHERE email = ? and password = ?",[email,password],function(error,results,fields){
+        if (results?.length > 0) {
+            res.redirect("templates", 301);
+        } 
+       else{
+            res.send("email or code khaser")
+           
+        }
+    });
+    
+    
+});
+app.post("/register", function(req,res){
+    const { fullname, email, password} = req.body;
+    conn.query('INSERT INTO signup SET ?', {username:fullname , email: email, password: password},(error,results) =>{
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(results);
+        }
+
+    })
+
+    res.render("index")
+});
+
+app.listen(3000)
+
